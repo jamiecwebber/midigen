7979e41b36a196635e153da15050aa7174a9bd87
Jamie Christopher Webber <jamiecwebber@gmail.com>
2019-09-23 16:23:11 -0400

    making spectraliser class

--- melody_spectralizer.py
+++ melody_spectralizer.py
@@ -13,24 +13,43 @@ from spectral_tools import *
 mid = MidiFile('formidigennosus.mid')
 
 
-dyad_note = None  # value of the first of the two notes of the dyad 
-
-def handle_msg(msg):
+class Spectralizer():
     # processes each message of the MIDI
-    if msg.channel == 1:
-        if dyad_note == None:
-            dyad_note = msg.note
+    def __init__(self):
+        self.dyad_note = None
+        self.generator = None
+    
+    
+        
+    def handle_msg(self, msg):
+        if msg.channel == 1:
+            print(msg)
+            if dyad_note == None:
+                dyad_note = msg.note
+            else:
+                self.change_generator(dyad_note, msg.note)
+                dyad_note = None
         else:
-            change_generator(dyad_note, msg.note)
-    else:
-        adjust_note()
+            self.adjust_note(msg)
+    
+    def change_generator (self, note_1, note_2):
+        note_1, note_2 = [note_1, note_2]*100
+        print(f'{note_1}, {note_2}')
+    
+    def adjust_note(msg):
+        print('blah')
+        
+    
 
 
+spec = Spectralizer()
 
 for i, track in enumerate(mid.tracks):
+    dyad_note = None  # value of the first of the two notes of the dyad 
+    
     print(f'Track {i}: {track.name}')
     for msg in track:
-        if not msg.type == 'control_change':
-            handle_msg(msg)
+        if msg.type == 'note_on' or msg.type == 'note_off':
+            spec.handle_msg(msg)
             
 
