f4cae47dc0435a64fe07d40edfa0913975ad437d
Jamie Christopher Webber <jamiecwebber@gmail.com>
2019-09-23 19:15:54 -0400

    created pitch bend message

--- melody_spectralizer.py
+++ melody_spectralizer.py
@@ -11,14 +11,16 @@ from midigen import *
 from spectral_tools import *
 
 mid = MidiFile('formidigen.mid')
-
+output_midi = MidiFile()
+midi_track = MidiTrack()
+output_midi.tracks.append(midi_track)
 
 class Spectralizer():
     # processes each message of the MIDI
     def __init__(self):
         self.dyad_note = None
         self.generator = None
-        self.notes_on = {}
+        self.notes_on = {}   # dictionary of tuples
     
     def handle_msg(self, msg):
         if msg.channel == 1:
@@ -29,7 +31,8 @@ class Spectralizer():
                     self.change_generator(self.dyad_note, msg.note)
                     self.dyad_note = None
         else:
-            self.adjust_note(msg)
+            msg = self.adjust_note(msg)
+        return 
     
     def change_generator (self, note_1, note_2):
         note_1, note_2 = note_1*100, note_2*100
@@ -43,18 +46,13 @@ class Spectralizer():
             msg.note = self.notes_on.pop(msg.note)
         else:
             self.notes_on[msg.note] = self.calculate_note(msg)
-            write_note(msg)
+            msg.note = 
             
     def calculate_note(self, msg):
         print('calculate note')
-        return 50
+        midicents = msg.note * 100 - 50
+        return mc_to_midi_and_pitchbend(midicents)
     
-
-def write_note(msg):
-    print(f'write note {msg.note} to file');
-        
-    
-
 
 spec = Spectralizer()
 
@@ -63,7 +61,12 @@ for i, track in enumerate(mid.tracks):
     
     print(f'Track {i}: {track.name}')
     for msg in track:
+        messages = [msg]
         if msg.type == 'note_on' or msg.type == 'note_off':
-            spec.handle_msg(msg)
+            messages = spec.handle_msg(msg)
+        for message in messages:
+            midi_track.append(message)
+
+output_midi.save('formidigenout.mid')
             
 
